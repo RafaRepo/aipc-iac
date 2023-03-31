7affd4c4d0e86aba94e6fb0f7c5841ea7ec87563
rafa <rafeek8268@gmail.com>
2023-03-31 00:58:41 +0000

    Remove duplicated directory

--- workshop2/resource.tf
+++
@@ -1,9 +0,0 @@
-resource "digitalocean_droplet" "web1" {
-    image = "ubuntu-20-04-x64"
-    name = "web1"
-    region = "sgp1"
-    size = "s-2vcpu-2gb"
-    ssh_keys = [
-      data.digitalocean_ssh_key.aipc.id
-    ]
-}
